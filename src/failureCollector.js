410ed7103d46ead14bf5e5795f7ebac086e85667
Rauno Viskus <rauno56@gmail.com>
2023-05-11 10:02:56 +0300

    feat: implement a generic way to collect failures from checks

---
+++ src/failureCollector.js
@@ -0,0 +1,27 @@
+import * as nodeAssert from 'node:assert/strict';
+import Failure from './Failure.js';
+
+export default () => {
+	const failures = new Map();
+	const assert = {
+		collectFailures: () => {
+			return [...failures.values()];
+		},
+	};
+	const defineAssert = (method) => {
+		return assert[method] = (...args) => {
+			try {
+				return nodeAssert[method](...args);
+			} catch (err) {
+				// deduplicating all the "duplicate X" failures
+				const failure = new Failure({ message: err.message });
+				failures.set(failure.message, failure);
+			}
+		};
+	};
+	defineAssert('ok');
+	defineAssert('fail');
+	defineAssert('equal');
+
+	return assert;
+};
--- src/usage.js
+++ src/usage.js
@@ -2,35 +2,38 @@ import { inspect } from 'util';
 import * as nodeAssert from 'node:assert/strict';
 
 import Index from './Index.js';
+import failureCollector from './failureCollector.js';
 
-const formatPercentage = (count, total) => {
-	return `${(100 * count / total).toFixed(1)}%`;
+const formatPercentage = (ratio) => {
+	return `${(100 * ratio).toFixed(1)}%`;
 };
 
 const assertUsage = (definitions, usageStats) => {
 	nodeAssert.ok(definitions && typeof definitions, 'object');
 	nodeAssert.ok(Array.isArray(usageStats), `Expected array as usage stats. Got: ${inspect(usageStats)}`);
 
+	const assert = failureCollector();
 	const index = new Index();
 	// collect failures but ignore issues for compiling usage failures
 	index.build(definitions, true);
-	const failures = [];
 
+	// Check resources that are used, but missing from definitions
+	// Likely to be temporary resources.
 	for (const u of usageStats) {
 		const vhost = u.vhost;
 		if (u.exchange) {
 			if (!index.maps.exchange.has(u.exchange, vhost)) {
-				console.error('used but missing', 'E', vhost, u.exchange);
+				console.warn(`Warning: Used but missing exchange "${u.exchange}"" in "${vhost}"`);
 			}
 			if (!index.maps.queue.has(u.queue, vhost)) {
-				console.error('used but missing', 'Q', vhost, u.queue);
+				console.warn(`Warning: Used but missing queue "${u.queue}"" in "${vhost}"`);
 			}
 		} else if (u.queue) {
 			if (!index.maps.queue.has(u.queue, vhost)) {
-				console.error('used but missing', 'Q', vhost, u.queue);
+				console.warn(`Warning: Used but missing queue "${u.queue}"" in "${vhost}"`);
 			}
 		} else {
-			console.log('rest', u);
+			throw new Error('Unexpected usage record type');
 		}
 	}
 
@@ -47,29 +50,31 @@ const assertUsage = (definitions, usageStats) => {
 		} else if (u.queue) {
 			index.maps.queue.delete(u.queue, vhost);
 		} else {
-			console.log('rest', u);
+			throw new Error('Unexpected usage record type');
 		}
 	}
 
-	console.log('# vhosts');
 	for (const i of index.db.vhost.values()) {
-		console.log('empirically unused', 'V', i.name);
+		assert.fail(`Empirically unused vhost: ${i.name}`);
 	}
-	console.log('Unused ratio', formatPercentage([...index.db.vhost.values()].length, vhostSizeBefore));
+	const vhostRatio = [...index.db.vhost.values()].length / vhostSizeBefore;
+	assert.ok(vhostRatio < 0.3, `High ratio of unused vhost resources: ${formatPercentage(vhostRatio)}`);
 
 	console.log('# Exchanges');
 	for (const i of index.db.exchange.values()) {
-		console.log('empirically unused', 'E', i.vhost, i.name);
+		assert.fail(`Empirically unused exchange: "${i.name}" in "${i.vhost}"`);
 	}
-	console.log('Unused ratio', formatPercentage([...index.db.exchange.values()].length, exchangeSizeBefore));
+	const exchangeRatio = [...index.db.exchange.values()].length / exchangeSizeBefore;
+	assert.ok(exchangeRatio < 0.3, `High ratio of unused exchange resources: ${formatPercentage(exchangeRatio)}`);
 
 	console.log('# Queues');
 	for (const i of index.db.queue.values()) {
-		console.log('empirically unused', 'Q', i.vhost, i.name);
+		assert.fail(`Empirically unused queue: "${i.name}" in "${i.vhost}"`);
 	}
-	console.log('Unused ratio', formatPercentage([...index.db.queue.values()].length, queueSizeBefore));
+	const queueRatio = [...index.db.queue.values()].length / queueSizeBefore;
+	assert.ok(queueRatio < 0.3, `High ratio of unused queue resources: ${formatPercentage(queueRatio)}`);
 
-	return failures;
+	return assert.collectFailures();
 };
 
 export const validateUsage = (def, stats) => assertUsage(def, stats);
