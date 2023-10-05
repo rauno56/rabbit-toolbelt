a1002cc75c8248d315c210511cb366e928cb645e
Rauno Viskus <rauno56@gmail.com>
2023-10-05 14:24:38 +0300

    feat: diff users

--- src/diff.js
+++ src/diff.js
@@ -1,4 +1,5 @@
-import { isDeepStrictEqual } from 'util';
+import assert from 'assert/strict';
+import { isDeepStrictEqual, inspect } from 'util';
 
 import Index, { key } from './Index.js';
 
@@ -6,6 +7,9 @@ const diffMapsConsuming = (before, after) => {
 	const added = [];
 	const deleted = [];
 	const changed = [];
+
+	assert.equal(typeof before?.all, 'function', `Invalid before state: ${inspect(before)}`);
+	assert.equal(typeof after?.all, 'function', `Invalid after state: ${inspect(after)}`);
 
 	for (const afterItem of after.all()) {
 		const beforeItem = before.getByKey(key.resource(afterItem));
@@ -33,9 +37,9 @@ const diff = (beforeDef, afterDef) => {
 	const before = Index.fromDefinitions(beforeDef, false);
 	const after = Index.fromDefinitions(afterDef, false);
 
-	const added = { vhosts: [], queues: [], exchanges: [], bindings: [] };
-	const deleted = { vhosts: [], queues: [], exchanges: [], bindings: [] };
-	const changed = { vhosts: [], queues: [], exchanges: [], bindings: [] };
+	const added = { vhosts: [], queues: [], exchanges: [], bindings: [], users: [] };
+	const deleted = { vhosts: [], queues: [], exchanges: [], bindings: [], users: [] };
+	const changed = { vhosts: [], queues: [], exchanges: [], bindings: [], users: [] };
 
 	const collectDiff = (key, beforeMap, afterMap) => {
 		const changes = diffMapsConsuming(beforeMap, afterMap);
@@ -48,6 +52,7 @@ const diff = (beforeDef, afterDef) => {
 	collectDiff('queues', before.queue, after.queue);
 	collectDiff('exchanges', before.exchange, after.exchange);
 	collectDiff('bindings', before.binding, after.binding);
+	collectDiff('users', before.user, after.user);
 
 	return {
 		added,
