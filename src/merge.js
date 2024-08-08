e3caf612ce56225f9c208c64599734cd0119f143
Rauno Viskus <rauno56@gmail.com>
2024-08-08 15:33:43 +0300

    feat: error if no files provided to merge

--- src/merge.js
+++ src/merge.js
@@ -1,8 +1,11 @@
+import assert from 'node:assert/strict';
 import Index from './Index.js';
 import { readJSONSync } from './utils.js';
 
 export const merge = (ignoreIndex = null, ...filePaths) => {
 	const index = new Index();
+
+	assert(filePaths.length, 'No files provided');
 
 	for (const filePath of filePaths) {
 		const definitions = readJSONSync(filePath);
